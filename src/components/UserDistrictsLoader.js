782d1e1266c9578a123c03ca4104c95000826785
Xavier Gillmann <xgillmann@bluesquarehub.com>
2019-11-25 14:17:40 +0100

    OMT-85 -> OMT-112 fixes

--- src/components/UserDistrictsLoader.js
+++ src/components/UserDistrictsLoader.js
@@ -5,16 +5,22 @@ import { fetchUserDistricts } from "../actions";
 
 class UserDistrictsLoader extends Component {
     componentDidMount() {
-        this.props.fetchUserDistricts();
+        if (!this.props.userDistricts) {
+            this.props.fetchUserDistricts();
+        }
     }
     render() {
         return null;
     }
 }
 
+const mapStateToProps = state => ({
+    userDistricts: !!state.loc && state.loc.userDistricts,
+});
+
 const mapDispatchToProps = dispatch => {
     return bindActionCreators({ fetchUserDistricts }, dispatch);
 };
 
 
-export default connect(null, mapDispatchToProps)(UserDistrictsLoader);
+export default connect(mapStateToProps, mapDispatchToProps)(UserDistrictsLoader);
